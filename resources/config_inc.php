c619f11ac46909e67194b1f08fd21ac69b268f5a
Anastasiia Petrochenkova <a.petrochenkova@ibolit.pro>
2022-04-28 14:55:37 +0300

    Ошибка

---
+++ resources/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'YBAeNGWGymQ6a9MK/UxabPWW3cREN2Sy+dm2MHt4aDs=';
+
+$g_use_capcha = off;
